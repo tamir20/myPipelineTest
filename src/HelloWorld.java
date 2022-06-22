3aab2d476f072c30ce1da115b740547a8119bd23
tamir20 <tamir20@users.noreply.github.com>
2022-06-22 19:37:34 +0300

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+    System.out.println("hello world");
   }
 }
