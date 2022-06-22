89b981eb43a199d647ac70cf43295fa3755d5d28
tamir20 <tamir20@users.noreply.github.com>
2022-06-22 19:33:40 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+package hello;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
 }
